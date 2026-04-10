a2dfc814577161eae669bb4d6a7a4278c646368d
Faisal <you@example.com>
2026-04-10 16:45:19 +0300

    d

--- fix_nav_logo.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const path = 'frontend/src/components/Navigation.jsx';
-let content = fs.readFileSync(path, 'utf8');
-
-content = content.replace("import { Home", "import logoUrl from '../assets/logo.svg';\nimport { Home");
-content = content.replace('src="/brain-bed.svg"', 'src={logoUrl}');
-
-fs.writeFileSync(path, content);
--- fix_setup_logo.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const path = 'frontend/src/pages/Setup.jsx';
-let content = fs.readFileSync(path, 'utf8');
-
-content = content.replace("import { Globe,", "import logoUrl from '../assets/logo.svg';\nimport { Globe,");
-content = content.replace('src="/brain-bed.svg"', 'src={logoUrl}');
-
-fs.writeFileSync(path, content);
